d82e1c9c6eb7a9a9b762c09d512156b9da906497
Yuvanraj <itsmeyuvanraj@gmail.com>
2025-02-07 18:59:52 +0530

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.light,
         getPages: AppPages.pages,
--- lib/presentations/marketplace/maket_place.dart
+++ lib/presentations/marketplace/maket_place.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:interview_task/core/assets/asset_images.dart';
+import 'package:interview_task/core/constants/app_colors.dart';
+import 'package:interview_task/core/constants/app_sizes.dart';
 import 'package:interview_task/presentations/widgets_common/custom_shapes/containers/rounded_container.dart';
 import 'package:interview_task/presentations/widgets_common/images/circular_image.dart';
 import 'package:interview_task/presentations/widgets_common/images/rounded_image.dart';
@@ -14,13 +17,14 @@ class MarketPlaceView extends StatelessWidget {
     return Column(
       children: [
         AppRoundedContainer(
+          padding: EdgeInsets.all(AppSizes.md),
           child: Column(
             children: [
               Row(
                 children: [
                   CachedNetworkImage(
-                    height: 55,
-                    width: 55,
+                    height: 65,
+                    width: 65,
                     imageUrl: AppImages.user,
                     imageBuilder: (context, imageProvider) => Container(
                       decoration: BoxDecoration(
@@ -28,8 +32,7 @@ class MarketPlaceView extends StatelessWidget {
                         image: DecorationImage(
                             image: imageProvider,
                             fit: BoxFit.fill,
-                            colorFilter:
-                            ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
+                           ),
                       ),
                     ),
                     progressIndicatorBuilder: (context, url, downloadProgress) => const AppShimmerEffect(width: 55, height: 55),
@@ -39,11 +42,39 @@ class MarketPlaceView extends StatelessWidget {
                         image: DecorationImage(
                             image: AssetImage(AppImages.user),
                             fit: BoxFit.fill,
-                            colorFilter:
-                            ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
+                            ),
                       ),
                     ),
                   ),
+                  SizedBox(width: AppSizes.lg),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Angel Rosser",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      SizedBox(
+                        width: Get.width / 1.6,
+                        child: Text('Sales Manager at Meesho privated limited',
+                        style: Theme.of(context).textTheme.titleMedium!.apply(color: AppColors.appGrey.withValues(alpha: .5)),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.history,
+                            color: AppColors.appGrey.withValues(alpha: .4),
+                          ),
+                          Text('2s',style: Theme.of(context).textTheme.labelMedium!.apply(color: AppColors.appGrey.withValues(alpha: .8),),)
+                        ],
+                      )
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios)
               ],)
             ],
           ),
